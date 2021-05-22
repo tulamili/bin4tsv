514468dd5f50024c886e4d24952480faa43499ea
Toshiyuki Shimono <bin4tsv@gmail.com>
2021-05-23 02:40:05 +0900

    27個のファイルを追加

---
+++ lib/bin4tsv.pm
@@ -0,0 +1,22 @@
+package bin4tsv ; 
+
+our $VERSION = 0.012 ; 
+our $DATE = '2021-05-22' ; 
+=head1 NAME
+ 
+bin4tsv
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+ 
+=head1 SEE ALSO
+
+bin4tsv::ja
+bin4tsv::en 
+
+
+=cut
+
+
+1 ; 
